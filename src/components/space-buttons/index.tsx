38b90d8812cf040a2d6e9b4429cc94df563a5404
yassinebenarbia <yassine.bna@proton.me>
2026-02-02 09:11:50 +0100

    fix: phone view buttons alignment

--- src/components/space-buttons/index.tsx
+++ src/components/space-buttons/index.tsx
@@ -10,21 +10,26 @@ const SpaceButtons = ({
   googleAnalyticId?: string;
 }) => {
   return (
-    < div className="flex justify-start py-2 px-1 items-center" >
-      <SpaceButton
-        externalProjects={[]}
-        header={blogs_header}
-        href={"/#/blogs"}
-        loading={false}
-      />
-      <SpaceButton
-        externalProjects={[]}
-        header={books_header}
-        href={"/#/books"}
-        googleAnalyticId={googleAnalyticId}
-        loading={false}
-      />
-    </div >
+    <div className="flex flex-col xl:flex-row items-center justify-center gap-1 py-2 px-1 w-full">
+      <div className="flex-grow font-medium flex items-center">
+        <SpaceButton
+          externalProjects={[]}
+          header={blogs_header}
+          href={"/#/blogs"}
+          loading={false}
+        />
+      </div>
+
+      <div className="flex-grow justify-start px-1 items-center">
+        <SpaceButton
+          externalProjects={[]}
+          header={books_header}
+          href={"/#/books"}
+          googleAnalyticId={googleAnalyticId}
+          loading={false}
+        />
+      </div>
+    </div>
   )
 }
 
